f602d04f7e53cef123bb166370bc13f091b8571c
tyebean <flower.webster@gmail.com>
2022-07-11 09:58:27 -0400

    hello world

---
+++ app.js
@@ -0,0 +1,11 @@
+// suppose we want to write a function that calculates the sum of all numbers from 1 up to (and including) some number n.
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= 1n; i++) {
+  total += i;
+  }
+  return total
+}
+console.log(addUpTo(6));
+console.log("hello world");
