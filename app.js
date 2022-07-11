8d6bf27c9319dc36b8723aa34a832d2c55a92559
tyebean <flower.webster@gmail.com>
2022-07-11 10:22:04 -0400

    add notes

--- app.js
+++ app.js
@@ -2,10 +2,30 @@
 
 function addUpTo(n) {
   let total = 0;
-  for (let i = 1; i <= 1n; i++) {
+  for (let i = 1; i <= n; i++) {
   total += i;
   }
   return total
 }
 console.log(addUpTo(6));
-console.log("hello world");
+
+function addUpTo2(n) {
+  return n * ( n + 1 ) / 2;
+}
+console.log(addUpTo2(6));
+
+//Which one is better?
+//What is better? Speed, less memory-intensive? Most legible?
+//Depends on the project, but speed and memory is most important
+
+//You can check the speed of your code-- not very reliable though, because every machine will compute these functions differently 
+  //The SAME machine can record time differently too so there's that
+//It can also difficult to quantify
+
+//Instead of computing the amount of seconds that it takes for a function to run, we can count the amount of operations that are happening.
+
+//There are 3 calculations/simple operations that are happeniong in addUpToTwo2
+
+//In addUpToZTwo, there are n operations. We don't want to focus on how many operations this one does, because it changes as n changes since this function contains a LOOPY LOOP. That's kind of aweful!
+//If n is 20, we'd loop over 20 -- 20 times. 🙀🙀🙀
+
