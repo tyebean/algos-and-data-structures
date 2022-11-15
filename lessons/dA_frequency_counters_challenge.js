6949f45199cb1b7264ff7fb4fd41da42a2b3f092
tyebean <flower.webster@gmail.com>
2022-11-15 16:48:03 -0500

    valid anagram algo challenge

---
+++ lessons/dA_frequency_counters_challenge.js
@@ -0,0 +1,17 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+function validAnagram(arr1, arr2) {
+  if (arr1.length != arr2.length){
+    return false
+  }
+  
+}
+
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram("rat","car") // false) // false
+validAnagram('awesome', 'awesom') // false
+validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+validAnagram('qwerty', 'qeywrt') // true
+validAnagram('texttwisttime', 'timetwisttext') // true
